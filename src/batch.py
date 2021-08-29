7266b1f31cd7d34e112b3f1514bc941da1b520e8
Takanori Itabashi <tknr@TakanorinoMacBook-Air.local>
2021-08-29 19:55:58 +0900

    db系削除

--- src/batch.py
+++ src/batch.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(__file__))
 Write your module
 Ex. import hoge
 """
-from logger.logging import log, get_logger  # noqa: E402
+from entity.logger.logging import log, get_logger  # noqa: E402
 
 logger = get_logger()
 
